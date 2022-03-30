3be3c3f160defc7c5c145e740eacb8f13ade2451
wesam1999 <wesam.qandah1999@gmail.com>
2022-03-30 21:07:41 +0300

    solved code challenge 15 and 16

--- trees/lib/src/main/java/trees/structure/BinaryTree.java
+++ trees/lib/src/main/java/trees/structure/BinaryTree.java
@@ -2,7 +2,10 @@ package trees.structure;
 
 import trees.data.BTNode;
 
+import java.util.LinkedList;
+
 public class BinaryTree {
+
     private BTNode root;
 
     public BTNode getRoot() {
@@ -13,4 +16,39 @@ public class BinaryTree {
         this.root = root;
     }
 
+    public void preOrderTraversalLoop() {
+        if (root != null) {
+            Queue<BTNode> queue = new Queue<>();
+            queue.enqueue(root);
+
+            BTNode node;
+            while (!queue.isQueueEmpty()) {
+                node = queue.dequeue();
+                System.out.print(node.getData() + " , ");
+                if (node.getLeft() != null) {
+                    queue.enqueue(node.getLeft());
+                }
+
+                if (node.getRight() != null) {
+                    queue.enqueue(node.getRight());
+                }
+            }
+        } else {
+            System.out.println("the tree is empty");
+        }
+    }
+
+    public void inordeTraversalloop() {
+        if (root != null) {
+            Queue<BTNode> queue = new Queue<>();
+            queue.enqueue(root);
+            levelOrderTraversalRecursive(queue);
+        } else {
+            System.out.println("the tree is empty ");
+        }
+    }
+
+    private void postorderTraversalloop(Queue<BTNode> queue) {
+
+    }
 }
